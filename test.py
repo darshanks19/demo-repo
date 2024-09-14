023e71371dc8f8cc6ec4102b8e374336415a72a5
Darshan K S <68590547+darshanks19@users.noreply.github.com>
2024-09-14 17:36:35 +0000

    learning strings manupilation in python

---
+++ test.py
@@ -0,0 +1,13 @@
+str1 = "i am / a devops / engineer"
+str2 = "Darshan"
+# Inbuilt split function
+cut = (str1.split("/")[0])
+
+print(cut)
+
+#inbuilt function upper to make the string in Uppercase letters
+print(cut.upper())
+
+#String concatination 
+concat = (cut + " " + str2)
+print(concat)
